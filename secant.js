30f6a5613f54550b4327ac93b65167edf6afd4e3
Changwoo.starshell <changwoo@starshell.co>
2017-08-23 13:23:49 +0900

    delete unused files

--- secant.js
+++
@@ -1,4 +0,0 @@
-var algos = require( "algos" ) ;
-var x0 = algos.bisectionSolver( function(x) { return Math.exp(x) + x*x - 10 } ) ;
-
-console.log(x0);
--- test.js
+++
@@ -1,7 +0,0 @@
-const mdl = require('./js/mdl');
-const algos = require('algos');
-
-mdl.setET(-0.76646);
-
-// console.log(mdl.mdl(21.73913043478261));
-console.log(algos.secantSolver(mdl.mdl, 27));
